0369f48e84d977959952c163a62ed8ab0f1867c7
Arnab Maiti <arnabmaiti007@gmail.com>
2020-11-17 10:34:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,34 +24,16 @@ def predict():
     input = [float(i.strip()) for i in request.form.values()]
     features = np.array([input])
     
-    print(features)
-    print(10)
-    
     columns_name = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX',
        'PTRATIO', 'B', 'LSTAT']
     
-    print(100)
-    
     df = pd.DataFrame(features, columns=columns_name)
     
-    print(200)
-    
-    #my_pipeline = Pipeline([
-    #    ('imputer', SimpleImputer(strategy="median")), # For handling missing values
-    #    ('std_scaler', StandardScaler())    # For Feature Scaling
-    #])
-    print(df)
-    
-    
     df = pipeline.transform(df)
-    
-    print(df)
-    
+     
     output = model.predict(df)
-    
-    print(output)
-    print(500)
+
     return render_template('index.html', prediction_text="Predicted Price: $%.2f"%(output[0]*1000))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
